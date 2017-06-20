73ba628d147c3248616495cae644b052a38b9ccd
jimdrake55x <jimdrake55x@gmail.com>
2017-06-20 08:18:23 -0500

    User Connection Events

--- Socket/index.js
+++ Socket/index.js
@@ -1,8 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/html/index.html');
+});
+
+io.on('connection',function(socket){
+    console.log('A User Connected');
+    socket.on('disconnect', function(){
+       console.log('A User has Disconnected'); 
+    });
 });
 
 http.listen(3000,function(){
