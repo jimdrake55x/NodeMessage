ba9270c4049ba92178ceb554a709df79ebf99bfe
jimdrake55x <jimdrake55x@gmail.com>
2017-07-05 12:01:29 -0500

    Groundwork for usernames laid

--- Socket/static/js/index/main.js
+++ Socket/static/js/index/main.js
@@ -1,16 +1,21 @@
 $(function(){
             var socket = io();
-            
+            var userNickname = "Default";
             $('#messageForm').submit(function(){
                 var MessageToSend = CryptoJS.AES.encrypt($('#Message').val(),$('#MessageKey').val());
                 socket.emit('chat message',MessageToSend.toString());
-                $('#messages').append($('<li class="list-group-item">').text(($('#Message').val())));
+                $('#messages').append($('<li class="list-group-item">').text(userNickname + ' : ' + ($('#Message').val())));
                 return false;
+            });
+    
+            $('#nickname-save').click(function(){
+                userNickname = $('#nickname-Name').val();
+                $('#nickNameModal').modal('hide');
             });
             
             socket.on('chat message',function(msg){
                 var RecievedMessage = CryptoJS.AES.decrypt(msg,$('#MessageKey').val()).toString(CryptoJS.enc.Utf8);
-                $('#messages').append($('<li class="list-group-item">').text(RecievedMessage));
+                $('#messages').append($('<li class="list-group-item">').text(userNickname + ' : ' +RecievedMessage));
             });
             
             socket.on('user connect',function(msg){
