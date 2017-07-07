031e9c4b971434d64ba66b6722f594bdc3150af0
jimdrake55x <jimdrake55x@gmail.com>
2017-07-07 12:56:11 -0500

    Added some markdown support

--- Socket/static/js/index/main.js
+++ Socket/static/js/index/main.js
@@ -8,8 +8,11 @@ $(function(){
 
                 var Message = {message:$('#Message').val(), sender:userNickname};
                 var MessageToSend = CryptoJS.AES.encrypt(JSON.stringify(Message),$('#MessageKey').val());
+                
                 socket.emit('chat message',MessageToSend.toString());
-                $('#messages').append($('<li class="list-group-item">').text('You : ' + ($('#Message').val())));
+
+                $('#messages').append($('<li class="list-group-item">').append(markdown.toHTML($('#Message').val())));
+                
                 $('#Message').val('');
                 $(document).scrollTop($(document).height());
                 return false;
@@ -25,7 +28,7 @@ $(function(){
             socket.on('chat message',function(msg){
                 var RecievedMessage = CryptoJS.AES.decrypt(msg,$('#MessageKey').val()).toString(CryptoJS.enc.Utf8);
                 RecievedMessage = JSON.parse(RecievedMessage);
-                $('#messages').append($('<li class="list-group-item">').text(RecievedMessage.sender + ' : ' + RecievedMessage.message));
+                $('#messages').append($('<li class="list-group-item">').append(markdown.toHTML(RecievedMessage.sender + ' : ' + RecievedMessage.message)));
             });
             
             //Receiving a user connect event. 
