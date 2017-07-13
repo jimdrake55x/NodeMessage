8df0615dd51451e36f46f147d5bf10df4dc8d847
jimdrake55x <jimdrake55x@gmail.com>
2017-07-13 13:04:25 -0500

    Markdown implemented

--- Socket/static/js/index/main.js
+++ Socket/static/js/index/main.js
@@ -1,6 +1,6 @@
 $(function(){
             var socket = io();
-            var userNickname = "Default";
+            var userNickname = "Somebody...";
             
 
             // Sending a Message
@@ -11,10 +11,19 @@ $(function(){
                 
                 socket.emit('chat message',MessageToSend.toString());
 
-                $('#messages').append($('<li class="list-group-item">').append(markdown.toHTML($('#Message').val())));
+                $('#messages').append($('<div class="row list-group-item">')
+                                .append($('<div class="col-md-1">')
+                                        .text("You :"))
+                                .append($('<div class="col-md-11">')
+                                    .append(markdown.toHTML(Message.message))));
                 
+                //Resetting Message Box
                 $('#Message').val('');
+
+                //Autoscroll to bottom
                 $(document).scrollTop($(document).height());
+
+                //Preventing Callback
                 return false;
             });
 
@@ -28,12 +37,17 @@ $(function(){
             socket.on('chat message',function(msg){
                 var RecievedMessage = CryptoJS.AES.decrypt(msg,$('#MessageKey').val()).toString(CryptoJS.enc.Utf8);
                 RecievedMessage = JSON.parse(RecievedMessage);
-                $('#messages').append($('<li class="list-group-item">').append(markdown.toHTML(RecievedMessage.sender + ' : ' + RecievedMessage.message)));
+
+                $('#messages').append($('<div class="row list-group-item">')
+                    .append($('<div class="col-md-1">')
+                        .text(RecievedMessage.sender + ' :'))
+                    .append($('<div class="col-md-11">')
+                        .append(markdown.toHTML(RecievedMessage.message))));
             });
             
             //Receiving a user connect event. 
             socket.on('user connect',function(msg){
-                $('#messages').append($('<li class="list-group-item list-group-item-info">').text(msg));
+                $('#messages').append($('<div class="row list-group-item list-group-item-info">').text(msg));
             });
         });
 
